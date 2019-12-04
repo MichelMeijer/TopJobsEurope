40820d4dcfa403df017aabe22b688b71f1cae4ea
Michel <michelmeijer16@hotmail.com>
2019-12-05 00:07:37 +0100

    Jobpage changed

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -2,13 +2,29 @@
 
 @section('content')
     <!-- {{ $job }} -->
-    <h1>{{ $job->title}}</h1>
-    <p><b>City: </b>{{ $job->city}}</p>
-    <p><b>Country: </b>{{ $job->country}}</p>
-    <p><b>Salary: </b>{{ $job->salary}}</p>
-    <p><b>Language: </b>{{ $job->language}}</p>
-    <p><b>Date posted: </b>{{ $job->creation_date}}</p>
-    <div><b>Full Description: </b>{!! $job->description !!}</div>
-    <a href="{{ $job->url }}" class="btn btn-primary">Apply!</a>
+    <div class="jobinfo">
+    <h1 class="jobtitle">{{ $job->title}}</h1> 
+    <hr>
+    <div class="row justify-content-center">
+    <div class="col-sm-6">
+    <h2><b>City: </b>{{ $job->city}}</h2>
+</div>
+    <div class="col-sm-6">
+    <h2><b>Country: </b>{{ $job->country}}</h2>
+</div>
+    <div class="col-sm-6">
+    <h2><b>Language: </b>{{ $job->language}}</h2>
+</div>
+<div class="col-sm-6">
+        <h2><b>Salary: </b>{{ $job->salary}}</h2>
+    </div>
+    </div>
+ <div class="column justify-content-center">
+        <h2><b>Date posted: </b>{{ $job->creation_date}}</h2>
+    </div><hr>
+    <div><h3><b>Full Description: </b>{!! $job->description !!}</h3></div>
+    <div class="row justify-content-center">
+    <a href="{{ $job->url }}" class="btn btn-primary1">Apply!</a></div><br><br><br><br><br><br><br><br>
+    </div>
 @endsection
 
--- storage/framework/views/4df3de05af36cce255a4499b502b8192bd7090e8.php
+++ storage/framework/views/4df3de05af36cce255a4499b502b8192bd7090e8.php
@@ -1,13 +1,29 @@
 <?php $__env->startSection('content'); ?>
     <!-- <?php echo e($job); ?> -->
-    <h1><?php echo e($job->title); ?></h1>
-    <p><b>City: </b><?php echo e($job->city); ?></p>
-    <p><b>Country: </b><?php echo e($job->country); ?></p>
-    <p><b>Salary: </b><?php echo e($job->salary); ?></p>
-    <p><b>Language: </b><?php echo e($job->language); ?></p>
-    <p><b>Date posted: </b><?php echo e($job->creation_date); ?></p>
-    <div><b>Full Description: </b><?php echo $job->description; ?></div>
-    <a href="<?php echo e($job->url); ?>" class="btn btn-primary">Apply!</a>
+    <div class="jobinfo">
+    <h1 class="jobtitle"><?php echo e($job->title); ?></h1> 
+    <hr>
+    <div class="row justify-content-center">
+    <div class="col-sm-6">
+    <h2><b>City: </b><?php echo e($job->city); ?></h2>
+</div>
+    <div class="col-sm-6">
+    <h2><b>Country: </b><?php echo e($job->country); ?></h2>
+</div>
+    <div class="col-sm-6">
+    <h2><b>Language: </b><?php echo e($job->language); ?></h2>
+</div>
+<div class="col-sm-6">
+        <h2><b>Salary: </b><?php echo e($job->salary); ?></h2>
+    </div>
+    </div>
+ <div class="column justify-content-center">
+        <h2><b>Date posted: </b><?php echo e($job->creation_date); ?></h2>
+    </div><hr>
+    <div><h3><b>Full Description: </b><?php echo $job->description; ?></h3></div>
+    <div class="row justify-content-center">
+    <a href="<?php echo e($job->url); ?>" class="btn btn-primary1">Apply!</a></div><br><br><br><br><br><br><br><br>
+    </div>
 <?php $__env->stopSection(); ?>
 
 
